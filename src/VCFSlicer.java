6189f527941c85a329f285144b0f5a20f1379991
Sam Hokin <shokin@ncgr.org>
2018-01-24 18:30:26 -0700

    Added Hamming distance on right to make it easier to sort the results.

--- src/VCFSlicer.java
+++ src/VCFSlicer.java
@@ -13,6 +13,8 @@ import java.util.Set;
 import java.util.Map;
 import java.util.LinkedHashMap;
 
+import org.apache.commons.text.similarity.HammingDistance;
+
 import htsjdk.samtools.SAMSequenceDictionary;
 import htsjdk.samtools.SAMSequenceRecord;
 import htsjdk.samtools.reference.ReferenceSequence;
@@ -28,7 +30,7 @@ import htsjdk.variant.vcf.VCFHeader;
 
 /**
  * Slice out a specific range of locations and samples with the specified zygotes and display in a stacked list of sequences.
- * Useful for seeing what the exact bases are for, say, the homozygous calls of a sample subgroup.
+ * Useful for seeing what the exact bases are for the heterozygous and homozygous calls of a sample subgroup.
  */
 public class VCFSlicer {
 
@@ -71,16 +73,19 @@ public class VCFSlicer {
             }
 
             // load desired samples as keys in a map of sequences 
-            Map<String,String> sequenceMap = new LinkedHashMap<String,String>();
+            Map<String,String> sequenceMap = new LinkedHashMap<String,String>();         // "-" for reference alleles
+            Map<String,String> fullSequenceMap = new LinkedHashMap<String,String>();     // actual base for calculation of distance
             BufferedReader samplesReader = new BufferedReader(new FileReader(samplesFile));
             String line = null;
             while ((line=samplesReader.readLine())!=null) {
                 sequenceMap.put(line, "");
+                fullSequenceMap.put(line, "");
             }
 
             // get the reference contig sequence
             IndexedFastaSequenceFile refSequenceFile = new IndexedFastaSequenceFile(refFile);
             ReferenceSequence refSequence = refSequenceFile.getSubsequenceAt(contig, start, end);
+            String refSequenceString = refSequence.getBaseString();
 
             // load VCF file
             VCFFileReader vcfReader = new VCFFileReader(vcfFile, true);
@@ -99,29 +104,42 @@ public class VCFSlicer {
                     for (String sample : sequenceMap.keySet()) {
                         
                         String sequence = sequenceMap.get(sample);
+                        String fullSequence = fullSequenceMap.get(sample);
                         Genotype g = vc.getGenotype(sample);
                         if (g.isHet()) {
                             sequence += g.getAllele(1).getBaseString().toLowerCase(); // 0=ref, 1=alt
+                            fullSequence += g.getAllele(1).getBaseString().toLowerCase(); // 0=ref, 1=alt
                         } else if (g.isHom()) {
                             sequence += g.getAllele(0).getBaseString().toUpperCase(); // 0=alt
+                            fullSequence += g.getAllele(0).getBaseString().toUpperCase(); // 0=alt
                         } else {
                             sequence += "-";
+                            fullSequence += refSequenceString.charAt(pos-start);
                         }
+                        // update the maps
                         sequenceMap.put(sample, sequence);
+                        fullSequenceMap.put(sample, fullSequence);
                     }
                     
                 } else {
-
+                    
                     for (String sample : sequenceMap.keySet()) {
                         String sequence = sequenceMap.get(sample);
+                        String fullSequence = fullSequenceMap.get(sample);
                         sequence += "-";
+                        fullSequence += refSequenceString.charAt(pos-start);
+                        // update the maps
                         sequenceMap.put(sample, sequence);
+                        fullSequenceMap.put(sample, fullSequence);
                     }
 
                 }
 
             }
 
+            // need to instantiate this for calculating distance from reference
+            HammingDistance ld = new HammingDistance();
+            
             // now output it all
             System.out.println("Reference:\t"+refFilename);
             System.out.println("VCF file:\t"+vcfFilename);
@@ -130,9 +148,10 @@ public class VCFSlicer {
             System.out.println("");
             System.out.println(contig+"\t"+start);
             System.out.println("\t"+"|");
-            System.out.println("REF\t"+refSequence.getBaseString());
+            System.out.println("REF\t"+refSequence.getBaseString()+" 0");
             for (String sample : sequenceMap.keySet()) {
-                System.out.println(sample+"\t"+sequenceMap.get(sample));
+                Integer distance = ld.apply(refSequenceString, fullSequenceMap.get(sample));
+                System.out.println(sample+"\t"+sequenceMap.get(sample)+" "+distance);
             }
             
         } catch (Exception ex) {
