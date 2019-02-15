19c5f4469f4401c1a1758c68a07576d42da5b7ba
Sam Hokin <shokin@ncgr.org>
2019-02-15 06:15:18 -0700

    Create a wig file from GFF/Fisher overlap.

--- src/main/java/edu/carnegiescience/dpb/evanslab/GFFSearcher.java
+++ src/main/java/edu/carnegiescience/dpb/evanslab/GFFSearcher.java
@@ -10,6 +10,12 @@ import org.biojava.nbio.genome.parsers.gff.Location;
 /**
  * Searches a GFF file for features that span given locations.
  *
+ * Location file has columns:
+ * contig	start	REF	ALT	a	b	c	d	size	p	mlog10p	signif
+ * 0            1       2       3       4       5       6       7       8       9       10      11
+ *
+ * Writes wig file format.
+ *
  * @author Sam Hokin
  */
 public class GFFSearcher {
@@ -29,22 +35,34 @@ public class GFFSearcher {
         GFFLoader loader = new GFFLoader(gffFilename);
 
         BufferedReader reader = new BufferedReader(new FileReader(locFilename));
+        int oldPos = 0;
+        String oldContig = "";
         String line;
         while ((line=reader.readLine())!=null) {
+            if (line.startsWith("#")) {
+                continue; // comment
+            } else if (line.toLowerCase().startsWith("contig")) {
+                continue; // heading
+            }
             String[] parts = line.split("\t");
-            String seqname = parts[0];
+            String contig = parts[0];
+            if (!contig.equals(oldContig)) {
+                System.out.println("variableStep chrom="+contig);
+                oldContig = contig;
+            }
             int pos = Integer.parseInt(parts[1]);
-            Location location = new Location(pos,pos);
-            FeatureList overlapping = loader.search(seqname, location);
-            if (overlapping.size()>0) {
-                // ONLY SPIT OUT THE FIRST MATCH!!!
-                FeatureI feature = overlapping.get(0);
-                System.out.println(seqname+"\t"+pos+"\t"+feature.getAttribute("ID")+"\t"+feature.getAttribute("Name"));
-            } else {
-                System.out.println(seqname+"\t"+pos+"\t"+"\t");
-            }                
+            String mlog10p = parts[10];
+            if (pos!=oldPos) {
+                oldPos = pos;
+                Location location = new Location(pos,pos);
+                FeatureList overlapping = loader.search(contig, location);
+                if (overlapping.size()>0) {
+                    FeatureI feature = overlapping.get(0);
+                    System.out.println(pos+"\t"+mlog10p);
+                }
+            }
         }
-        
+        reader.close();
     }
 
 }
