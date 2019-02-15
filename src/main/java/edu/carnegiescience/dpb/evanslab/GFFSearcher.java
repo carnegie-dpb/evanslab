7d063455cc10199dc6e988687ee5a2b7bf85e28f
Sam Hokin <shokin@ncgr.org>
2019-02-15 08:31:40 -0700

    Parallelize? Need lots of RAM.

--- src/main/java/edu/carnegiescience/dpb/evanslab/GFFSearcher.java
+++ src/main/java/edu/carnegiescience/dpb/evanslab/GFFSearcher.java
@@ -2,6 +2,7 @@ package edu.carnegiescience.dpb.evanslab;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.biojava.nbio.genome.parsers.gff.FeatureI;
 import org.biojava.nbio.genome.parsers.gff.FeatureList;
@@ -10,7 +11,7 @@ import org.biojava.nbio.genome.parsers.gff.Location;
 /**
  * Searches a GFF file for features that span given locations.
  *
- * Location file has columns:
+ * Location file from a Fisher run has columns:
  * contig	start	REF	ALT	a	b	c	d	size	p	mlog10p	signif
  * 0            1       2       3       4       5       6       7       8       9       10      11
  *
@@ -20,13 +21,10 @@ import org.biojava.nbio.genome.parsers.gff.Location;
  */
 public class GFFSearcher {
 
-    /**
-     * Find features that overlap locations in a provided location file.
-     */
     public static void main(String[] args) throws Exception {
 
         if (args.length!=2) {
-            System.out.println("Usage GFFSearcher <gff-file> <location-file>");
+            System.out.println("Usage GFFSearcher <gff-file> <locations-file>");
             System.exit(0);
         }
         String gffFilename = args[0];
@@ -34,9 +32,11 @@ public class GFFSearcher {
 
         GFFLoader loader = new GFFLoader(gffFilename);
 
+        // load the positions and values in from the locations file
         BufferedReader reader = new BufferedReader(new FileReader(locFilename));
-        int oldPos = 0;
+        ConcurrentHashMap<String,String> locValues = new ConcurrentHashMap<>();
         String oldContig = "";
+        int oldPos = 0;
         String line;
         while ((line=reader.readLine())!=null) {
             if (line.startsWith("#")) {
@@ -46,23 +46,31 @@ public class GFFSearcher {
             }
             String[] parts = line.split("\t");
             String contig = parts[0];
-            if (!contig.equals(oldContig)) {
-                System.out.println("variableStep chrom="+contig);
-                oldContig = contig;
-            }
             int pos = Integer.parseInt(parts[1]);
-            String mlog10p = parts[10];
+            String value = parts[10];
             if (pos!=oldPos) {
                 oldPos = pos;
-                Location location = new Location(pos,pos);
-                FeatureList overlapping = loader.search(contig, location);
-                if (overlapping.size()>0) {
-                    FeatureI feature = overlapping.get(0);
-                    System.out.println(pos+"\t"+mlog10p);
-                }
+                locValues.put(contig+":"+pos, value);
             }
         }
         reader.close();
+
+        System.out.println("locValues.size()="+locValues.size());
+
+
+        
     }
+
+    // if (!contig.equals(oldContig)) {
+    //     oldContig = contig;
+    //     System.out.println("variableStep chrom="+contig);
+    // }
+    // Location location = new Location(pos,pos);
+    // FeatureList overlapping = loader.search(contig, location);
+    // if (overlapping.size()>0) {
+    //     FeatureI feature = overlapping.get(0);
+    //     System.out.println(pos+"\t"+mlog10p);
+    // }
+
 
 }
